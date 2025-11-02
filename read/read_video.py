084589fea13a9e01188edb4173a4a617853023ea
raj14428o <rajkesharwani14428o@gmail.com>
2025-11-02 17:51:59 +0530

    Fix import statement formatting in read_video.py

--- read/read_video.py
+++ read/read_video.py
@@ -1,8 +1,8 @@
-import cv2 as cv # type: ignore
+import cv2 as cv 
 
 capture=cv.VideoCapture(0)
 while True:
-    isTrue, frame = capture.read(0)  ## you can also give file path also
+    isTrue, frame = capture.read(0)  ## you can also give file path also 
     cv.imshow('Video',frame)
 
     if cv.waitKey(20) & 0xFF==ord('d'):
